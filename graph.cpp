0c4e986a9d65ba5819e59ca38df562f769fbf8dc
Justin Sadler <jsad@scc1.bu.edu>
2023-05-04 19:12:53 -0400

    Added new CSV file and new changes to graph generator

--- graph.cpp
+++ graph.cpp
@@ -16,54 +16,69 @@ Sources:
 #include <stdlib.h>
 #include <vector>
 
+#if 0
+#define LOG(X) std::cout << (X) << std::endl
+#else
+#define LOG(X) 
+#endif
+
 using std::vector;
 
 vector<vector<int>> GenRandomGraphs(unsigned int V, unsigned int E, unsigned int maxCap);
+void errorMessage(const char * msg);
 
 int main( int argc, char* argv[] ) {
 
     std::ofstream myfile;
+    const char * filename = "example.csv";
     unsigned long numberOfNodes;
     unsigned long maxEdges;
     unsigned long minEdges;
     unsigned long numberOfEdges;
     unsigned long maxCapacity;
     vector<vector<int>> edgeList;
-    myfile.open("example.csv");
 
     if(argc < 4) {
-        std::cerr << "ERROR: Insufficient number of arguments" << std::endl;
-        std::cerr << "[Format]: ./graph [No. of nodes] [sparse|dense] [Max Capacity]" << std::endl;
+        errorMessage("ERROR: Insufficient number of arguments");
         exit(-1);
     }
 
     if((numberOfNodes = strtoul(argv[1], NULL, 10)) == 0) {
-        std::cerr << "ERROR: Could not extract the number of nodes from command line arguments" << std::endl;
-        std::cerr << "[Format]: ./graph [No. of nodes] [sparse|dense] [Max Capacity]" << std::endl;
+	errorMessage("ERROR: Could not extract the number of nodes from command line arguments" );
         exit(-1);
+    }
+
+    if(numberOfNodes <= 2) {
+	errorMessage("Error: Less than 3 nodes in graph. Problem is trivial");
+	exit(-1);
     }
 
     if(strcmp(argv[2], "dense") == 0) {
-        maxEdges = numberOfNodes * (numberOfNodes - 1);
-        minEdges = maxEdges / 2;
-    } else if(strcmp(argv[2], "sparse")) {
         maxEdges = numberOfNodes * (numberOfNodes - 1) / 2;
+        minEdges = maxEdges / 2;
+    } else if(strcmp(argv[2], "sparse") == 0) {
+        maxEdges = numberOfNodes * (numberOfNodes - 1) / 4;
         minEdges = numberOfNodes - 1;
     } else {
-        std::cerr << "ERROR: incorrect option for 2nd command line arguement" << std::endl;
-        std::cerr << "[Format]: ./graph [No. of nodes] [sparse|dense] [Max Capacity]" << std::endl;
+	errorMessage("ERROR: incorrect option for 2nd command line arguement" );
         exit(-1);
     }
 
+    srand(time(NULL));
     numberOfEdges = (rand() % (maxEdges - minEdges)) + minEdges;
 
     if((maxCapacity = strtoul(argv[3], NULL, 10)) == 0) {
-        std::cerr << "ERROR: Could not extract max capacity from command line arguments" << std::endl;
-        std::cerr << "[Format]: ./graph [No. of nodes] [sparse|dense] [Max Capacity]" << std::endl;
+	errorMessage("ERROR: Could not extract max capacity from command line arguments");
         exit(-1);
     }
 
+    if(argc > 5) {
+		filename = argv[4];
+    }
+
     edgeList = GenRandomGraphs(numberOfNodes, numberOfEdges, maxCapacity);
+
+    myfile.open(filename);
 
     myfile << "source,destination,capacity\n";
     for(vector<int>& edge : edgeList) {
@@ -77,37 +92,88 @@ int main( int argc, char* argv[] ) {
 // C++ code to implement the approach
   
 // Function to generate random graph
+// Assumes V > 2 and E > V
 vector<vector<int>> GenRandomGraphs(unsigned int V, unsigned int E, unsigned int maxCap)
 {
     
-    int i, j, count;
+    int i, j, k;
     // Edge list
     vector<vector<int>> edges(E, vector<int> (3, 0));
     i = 0;
+    int source = 0;
+    int sink = V - 1;
+
+    srand(time(NULL));
+    
+	LOG("Intermediary nodes" );
   
     // Assign random values to the number
-    // of vertex and edges of the graph,
-    // Using rand().
-    while (i < V) {
-    initEdge:
-        edges[i][0] = rand() % V + 1;
-        edges[i][1] = rand() % V + 1;
+    // Intermediary edges 
+    while (i < (E *.75) ) {
+    initEdge1:
+	
+        edges[i][0] = (rand() % (V - 2)) + 1;
+        edges[i][1] = (rand() % (V - 2)) + 1;
   
         // Print the connections of each
         // vertex, irrespective of the
         // direction.
         if(edges[i][0] == edges[i][1]) { // self-loop
-            goto initEdge;
+            goto initEdge1;
         } 
         for (j = 0; j < i; j++) {
             if ((edges[i][0] == edges[j][0] && edges[i][1] == edges[j][1]) // edge is a duplicate
-                || (edges[i][0] == edges[j][0])) { // 
-                    goto initEdge;
+                || (edges[i][0] == edges[j][1] && edges[i][1] == edges[j][0])) { // there is already an edge between i and j
+                    goto initEdge1;
                 }
         }
-        edges[i][3] = (rand() % (maxCap - 1)) + 1;
+        edges[i][2] = (rand() % (maxCap - 1)) + 1;
         i++;
     }
 
+	k = i;
+	LOG("Source edges");
+	// Source edges:
+	while (i < (E * .875)) {
+	initEdge2:
+		edges[i][0] = source;
+		edges[i][1] = (rand() % (V - 1)) + 1;
+
+		for (j = k; j < i; j++) {
+			if ( edges[i][1] == edges[j][1]) { // edge is a duplicat
+				LOG("Dup 2--");
+				LOG(i);
+				LOG(j);
+				goto initEdge2;
+			}
+		}
+		edges[i][2] = (rand() % (maxCap - 1)) + 1;
+		i++;
+	}
+
+
+	LOG("Sink edges");
+	k = i;
+	// Destination edges:
+	while (i < E) {
+        initEdge3:
+        edges[i][0] = (rand() % (V - 1));
+        edges[i][1] = sink;
+  
+        for (j = k; j < i; j++) {
+            if ( edges[i][0] == edges[j][0]) { // edge is a duplicate
+		    LOG("Dup 3");
+                    goto initEdge3;
+                }
+        }
+        edges[i][2] = (rand() % (maxCap - 1)) + 1;
+        i++;
+	}
+
     return edges;
+}
+
+void errorMessage(const char * msg) {
+    std::cerr << msg << std::endl;
+    std::cerr << "[Format]: ./graph [No. of nodes] [sparse|dense] [Max Capacity] [Filename (default is example.csv)]" << std::endl;
 }
